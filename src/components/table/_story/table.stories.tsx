279fb7157a56a078c564428adca9ef537cf80b61
gomi <gxy880520@qq.com>
2024-04-25 17:58:04 +0800

    chroe: add story config

--- src/components/table/_story/table.stories.tsx
+++ src/components/table/_story/table.stories.tsx
@@ -1,19 +1,21 @@
 import type { Meta, StoryObj } from '@storybook/vue3';
+import { within, userEvent, expect } from '@storybook/test';
 import { fn } from '@storybook/test';
-import { Table } from '../table';
+import Table from '../index.vue';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta = {
-  title: 'Example/Table',
-  component: Table,
-  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
-  argTypes: {
-    data: { control: 'select', options: ['small', 'medium', 'large'] },
-  },
-  args: {
-    data: [],
-  },
+	title: 'Components/Table',
+	component: Table,
+	// This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
+	tags: ['autodocs'],
+	argTypes: {
+		size: { control: 'select', options: ['small', 'medium', 'large'] },
+	},
+	args: {
+		data: [],
+		showHeader: true
+	},
 } satisfies Meta<typeof Table>;
 
 export default meta;
@@ -24,13 +26,34 @@ type Story = StoryObj<typeof meta>;
  * to learn how to use render functions.
  */
 export const Empty: Story = {
-  args: {
-    data: []
-  },
+	args: {
+		data: []
+	},
 };
 
+const data = [{ name: 'gxy', age: 12 }, { name: 'zh', age: 22, time: '2019' }, { name: 'zh', age: 22, time: '2019' }, { name: 'xx', age: 22, time: '2019' }, { name: 'zh', age: 22, time: '2019' }, { name: 'zh', age: 22, time: '2019' }];
+
 export const Normal: Story = {
-  args: {
-    data: ['s','b']
-  },
+	render: (args) => ({
+		components: { Table },
+		setup() {
+			return { args };
+		},
+		template: '<Table v-bind="args" />',
+	}),
+	args: {
+		data,
+		size: 'small',
+		showHeader: true
+	},
+	play: async ({ canvasElement }: any) => {
+		// 测试是否渲染
+		const canvas = within(canvasElement);
+		const table = canvas.getByRole('cf-table');
+		await expect(table).toBeInTheDocument();
+
+		//  测试渲染列是否正确
+		const col = document.querySelectorAll('.el-table__body col');
+		await expect(col.length).toEqual(2);
+	},
 };
---
+++ src/components/table/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <el-table role="cf-table" v-bind="props">
+        <el-table-column v-for="(column, index) in columns" :key="index" :prop="column" :lable="column"></el-table-column>
+    </el-table>
+</template>
+
+<script lang="ts">
+import { ElTable, ElTableColumn } from 'element-plus';
+import defaultProps from 'element-plus/es/components/table/src/table/defaults.mjs';
+import 'element-plus/es/components/table/style/css';
+
+const SFTableProps = () => defaultProps;
+
+export default {
+    name: 'CFTable',
+    props: SFTableProps(),
+    components: {
+        ElTable,
+        ElTableColumn
+    },
+    setup(props, {}) {
+        const columns = Object.keys(props.data[0] || {});
+        return {
+            columns,
+            props: props
+        };
+    }
+}
+
+
+
+</script>
